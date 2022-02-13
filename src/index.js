8b392081ed9e18d6bedf1cac36b3c8bcb2eb7fd3
rustymcfly <martin@DESKTOP-5PHTFDH.localdomain>
2022-02-13 19:09:43 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const bot_token = '25da6f4417419cb1b507fe099ed9dfca1e893de6fa8e7c487c55326b56d56540'
+
+import Discord from 'discord.js'
+
+const client = new Discord.Client()
+
+client.login(bot_token)
