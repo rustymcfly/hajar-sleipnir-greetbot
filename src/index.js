bd8cdbacfce056f44dff4e0e391652c87767243b
mflog <m.flogaus@googlemail.com>
2022-02-13 19:39:58 +0100

    implemented discord api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-const bot_token = '25da6f4417419cb1b507fe099ed9dfca1e893de6fa8e7c487c55326b56d56540'
 
-import Discord from 'discord.js'
 
-const client = new Discord.Client()
+const bot_token = 'OTQyNDc1MzI4MzI3MTQ3NjYw.YglCjA.I8k0Lub4T5OJaSAqJbRhkF8E2So'
 
-client.login(bot_token)
+import Discord, {Intents} from 'discord.js'
+
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+const prefix = "!";
+
+client.on("message", function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    }
+
+    else if (command === "sum") {
+        const numArgs = args.map(x => parseFloat(x));
+        const sum = numArgs.reduce((counter, x) => counter += x);
+        message.reply(`The sum of all the arguments you provided is ${sum}!`);
+    }
+});
+
+await client.login(bot_token)
